6d3aa4d0655be4c282474a4d92f777e8292badc2
sohantalukder <sohan15-1933@diu.edu.bd>
2025-05-26 12:18:42 +0600

    feat: Add PhoneInput component and CountryCode type to index export

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,2 +1,4 @@
-export { default as CountryPickerModal } from './main/country-picker/CountryPickerModal';
+export { default as PhoneInput } from './main/phone-input/PhoneInput';
 export type { PhoneInputProps, PhoneInputRef } from './index.d';
+
+export type { CountryCode } from './main/constants/constants.d';
